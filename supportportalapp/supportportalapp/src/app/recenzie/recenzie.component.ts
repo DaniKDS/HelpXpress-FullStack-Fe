bda8cad82c06a661db93ff12104fefa34d99737b
Daniel_Cordis <cordisdany@gmail.com>
2024-04-14 01:32:33 +0300

    programari

---
+++ supportportalapp/supportportalapp/src/app/recenzie/recenzie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recenzie',
+  templateUrl: './recenzie.component.html',
+  styleUrls: ['./recenzie.component.css']
+})
+export class RecenzieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ supportportalapp/supportportalapp/src/app/specialuser/specialuser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-specialuser',
+  templateUrl: './specialuser.component.html',
+  styleUrls: ['./specialuser.component.css']
+})
+export class SpecialuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
