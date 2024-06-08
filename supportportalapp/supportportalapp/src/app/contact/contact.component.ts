af38b448f99354f3caf4cb2375c1b99cb97812a8
Daniel_Cordis <cordisdany@gmail.com>
2024-06-08 17:19:47 +0300

    contact+review

--- supportportalapp/supportportalapp/src/app/contact/contact.component.ts
+++ supportportalapp/supportportalapp/src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {NotificationType} from "../enum/notification-type.enum";
+import {NotificationService} from "../service/notification.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
+  public name: string;
+  public email: any;
+  public message: any;
   ngOnInit(): void {
   }
+  constructor(private http: HttpClient, private notificationService: NotificationService) {}
 
+  submitContactForm() {
+    const contactData = {
+      name: this.name,
+      email: this.email,
+      message: this.message
+    };
+
+    this.http.post('http://localhost:8081/api/contact/sendContactEmail', contactData, { responseType: 'text' }).subscribe(
+      (response: string) => {
+        this.notificationService.notify(NotificationType.SUCCESS, 'Mesajul a fost trimis cu succes.');
+        this.resetForm();
+      },
+      error => {
+        console.error('Error:', error);
+        this.notificationService.notify(NotificationType.ERROR, 'A apărut o eroare la trimiterea mesajului.');
+      }
+    );
+  }
+
+  resetForm() {
+    this.name = '';
+    this.email = '';
+    this.message = '';
+  }
 }
--- supportportalapp/supportportalapp/src/app/recenzie/recenzie.component.ts
+++ supportportalapp/supportportalapp/src/app/recenzie/recenzie.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+// recenzie.component.ts
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {NotificationService} from '../service/notification.service';
+import {NotificationType} from '../enum/notification-type.enum';
 
 @Component({
   selector: 'app-recenzie',
   templateUrl: './recenzie.component.html',
   styleUrls: ['./recenzie.component.css']
 })
-export class RecenzieComponent implements OnInit {
+export class RecenzieComponent {
+  comment = '';
+  rating = '5';
+  doctor = '';
+  organization = '';
 
-  constructor() { }
+  constructor(private http: HttpClient, private notificationService: NotificationService) {}
 
-  ngOnInit(): void {
+  submitReviewForm() {
+    const reviewData = {
+      comment: this.comment,
+      rating: this.rating,
+      doctor: this.doctor,
+      organization: this.organization
+    };
+
+    this.http.post('http://localhost:8081/api/contact/sendReviewEmail', reviewData).subscribe(
+      (response: any) => {
+        this.notificationService.notify(NotificationType.SUCCESS, response.message);
+        this.resetForm();
+      },
+      (error: any) => {
+        console.error('Error:', error);
+        this.notificationService.notify(NotificationType.ERROR, 'A apărut o eroare la trimiterea recenziei.');
+      }
+    );
   }
 
+  resetForm() {
+    this.comment = '';
+    this.rating = '5';
+    this.doctor = '';
+    this.organization = '';
+  }
 }
